fcaa8f3123a467034769d4fa897682eb294b4de9
Sanjin Hadžalić <80854488+SaHaroza014@users.noreply.github.com>
2022-08-19 19:39:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import messagebox
-
 import work_data
 from work_data import *
 import pandas
@@ -12,6 +11,10 @@ root.geometry('500x500')
 root.configure(background='white', padx=20, pady=20, cursor="arrow", bg="white")
 
 file = pandas.read_csv('stored_data_birds.csv')
+bird_names = pandas.read_csv('Popis ptica Hrvatske_BIOM.csv')
+observers_list = pandas.read_csv("Book1.csv")
+observers_list_2 = observers_list.columns.to_list()
+OPTIONS = observers_list_2
 
 
 class NewWindow(Toplevel):
@@ -59,6 +62,42 @@ class NewWindow(Toplevel):
             all_time_label.config(bg=random.choice(colors), fg=random.choice(colors))
             root.config(bg=random.choice(colors))
 
+        class AddUser(Toplevel):
+
+            def __init__(self, master=None):
+                super().__init__(master=root)
+                self.title("Add/delete user(s)")
+                self.geometry("250x250")
+                add_user_1 = Label(self, text="Add user", padx=10, pady=10)
+                add_user_1.grid(row=0, column=0)
+                add_user_1_entry = Entry(self)
+                add_user_1_entry.grid(row=0, column=1)
+
+                def add_user_function():
+                    added = add_user_1_entry.get()
+                    messagebox.askyesno(title="Add user warning", message=f"Do you want add {added}")
+                    added_df = pandas.DataFrame({
+                        "observer": [added]
+                    })
+                    added_df.to_csv('Book1.csv', mode='a', index=False, header=False)
+                    add_user_1_entry.delete(0, END)
+
+                def delete_user_function():
+                    deleted = delete_user_1_entry.get()
+                    for index, row in observers_list.iterrows():
+                        if deleted == row['observer']:
+                            observers_list.drop(observers_list.index[observers_list['observer'] == deleted], inplace=True)
+                            delete_user_1_entry.delete(0, END)
+
+                add_user_1_bttn = Button(self, text="Add", command=add_user_function)
+                add_user_1_bttn.grid(row=0, column=2)
+                delete_user_1 = Label(self, text="Delete user", padx=10, pady=10)
+                delete_user_1.grid(row=1, column=0)
+                delete_user_1_entry = Entry(self)
+                delete_user_1_entry.grid(row=1, column=1)
+                delete_user_1_bttn = Button(self, text="Delete", command=delete_user_function)
+                delete_user_1_bttn.grid(row=1, column=2)
+
         dark_mode = Button(self, text="Dark mode", command=dark_mode)
         dark_mode.grid(row=1, column=0)
         light_mode = Button(self, text="Light mode", command=light_mode)
@@ -68,7 +107,7 @@ class NewWindow(Toplevel):
 
         user_label = Label(self, text="User configuration", pady=10, padx=10)
         user_label.grid(row=2, column=1)
-        user_config = Button(self, text="Add/Delete user(s)")
+        user_config = Button(self, text="Add user(s)", command=AddUser)
         user_config.grid(row=3, column=1)
 
 
@@ -100,7 +139,6 @@ all_time_label = Label(text="All input:")
 all_time_label.config(padx=10, pady=10, bg="white")
 all_time_label.place(relx=0.7, rely=0.55, anchor=CENTER)
 
-
 with open("score.txt", "r") as testr:
     score = int(testr.read())
 all_time_num = Label(text=score, bg="white")
@@ -108,26 +146,26 @@ all_time_num.place(relx=0.8, rely=0.55, anchor=CENTER)
 
 
 def add_data():
-    eng_bird = bird_species_entry.get()
-    sci_bird = ""
-    for value in work_data.bird_list:
-        if eng_bird == value:
-            sci_bird = work_data.bird_list[value]
+    sci_bird = bird_species_entry.get()
+    eng_real = ""
+    for index, row in bird_names.iterrows():
+        if sci_bird == row["Znanstveno ime"]:
+            eng_real = row["Englesko ime"]
     num_data = bird_num_entry.get()
     observer_data = observer.get()
     location_data = location.get()
     ring_data = ''
     for value in file['ring_num']:
-        if value == ring_num_entry.get():
+        while value == ring_num_entry.get():
             ring_num_entry.delete(0, END)
             messagebox.showerror(title="Error", message="entered ring number already exists in library\nPlease check "
                                                         "ring number\n")
         else:
             ring_data = ring_num_entry.get()
 
-    if len(eng_bird) > 0 and len(ring_data) > 5:
-        df = pandas.DataFrame({'eng_species': [eng_bird],
-                               'sci_species': [sci_bird],
+    if len(sci_bird) > 0 and len(ring_data) > 5:
+        df = pandas.DataFrame({'sci_species': [sci_bird],
+                               'eng_species': [eng_real],
                                'num_of_birds': [num_data],
                                'ring_num': [ring_data],
                                'Location': [location_data],
@@ -148,7 +186,6 @@ def add_data():
                 all_time_num.config(text=1 + highest)
                 hisc.seek(0)
                 hisc.write(str(1 + highest))
-            print(work_data.c_sInp)
     else:
         messagebox.showerror(title="Error!!!", message="Not a valid input!\nPlease try again\n")
 
@@ -161,13 +198,14 @@ location.set("Choose location")
 location_drop = OptionMenu(root, location, *locations)
 location_drop.place(relx=0.35, rely=0.45, anchor=CENTER)
 
-observer = StringVar()
-observer.set("Choose observer")
-observer_drop = OptionMenu(root, observer, *observers)
+observer = StringVar(root)
+observer.set(OPTIONS[0])
+observer_drop = OptionMenu(root, observer, *observers_list['observer'])
 observer_drop.place(rely=0.55, relx=0.35, anchor=CENTER)
 
 configure_button = Button(root, text="Configure")
 configure_button.bind("<Button>", lambda e: NewWindow(root).focus())
 configure_button.place(rely=0.65, relx=0.65, anchor=CENTER)
+
 
 root.mainloop()
--- work_data.py
+++ work_data.py
@@ -1,15 +1,3 @@
-observers = [
-    "Luka Jurinović",
-    "Louie Thomas Taylor",
-    "Katarina Horvat",
-    "Biljana Ječmenica",
-    "Bariša Ilić",
-    "Sanjin Hadžalić",
-    "Jelena Kralj",
-    "Sandra Hodić",
-    "Petra Čulig"
-]
-
 locations ={
     "Vransko jezero": [43.9376735, 15.515467],
     "PP Učka": [45.2249757, 14.2267662],
@@ -31,16 +19,5 @@ colors = [
     "green",
     "brown"
 ]
-
-bird_list = { # bird sci/eng list for validating user input
-    "Gavia stellata": "Red-throated loon",
-    "Gavia arctica": "Black-throated loon",
-    "Gavia immer": "Common loon",
-    "Gavia adamsii": "Yellow-billed loon",
-    "Tachybaptus ruficollis": "Little grebe",
-    "Podiceps cristatus": "Great crested grebe",
-    "Podiceps grisigena": "Red-necked grebe"
-
-}
 
 c_sInp = int(0)
